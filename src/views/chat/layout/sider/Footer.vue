ed89dcb65b87b134c899bb2e9439e4e87dd3cf15
Mericol <2472482459@qq.com>
2023-04-20 15:45:13 +0800

    feat: 去除没使用的变量

--- src/views/chat/layout/sider/Footer.vue
+++ src/views/chat/layout/sider/Footer.vue
@@ -1,9 +1,7 @@
 <script setup lang='ts'>
-import { defineAsyncComponent, ref } from 'vue'
+// const Setting = defineAsyncComponent(() => import('@/components/common/Setting/index.vue'))
 
-const Setting = defineAsyncComponent(() => import('@/components/common/Setting/index.vue'))
-
-const show = ref(false)
+// const show = ref(false)
 </script>
 
 <template>
